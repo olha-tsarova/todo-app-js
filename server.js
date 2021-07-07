0cfeff573fae4f416631e4a38865a9d2316a5f6b
OlgaTs <alita.jet@gmail.com>
2021-07-07 18:59:51 +0300

    mongoose

--- server.js
+++ server.js
@@ -1,31 +1,47 @@
 const http = require('http')
 
-let todos = [{
-  id: 46556,
-  title: 'sdvgdxcfa',
-  completed: false
-}]
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true})
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function() {
+  console.log('Connected')
+});
+
+const todoSchema = new mongoose.Schema({
+  title: String,
+});
+
+const Todo = mongoose.model('Todo', todoSchema)
 
 const server = http.createServer((req, res) => {
-  const { headers, method, url } = req
-
+  const { method, url } = req
   console.log(`method: ${method} url: ${url}`);
+
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
 
   if (method === 'GET' && url === '/todos') {
-    res.end(JSON.stringify(todos));
+    res.write()
+    res.end()
   }
 
-  if (method === 'POST' && url === '/todos') {
+  if (method === 'POST' && url === '/addtodo') {
     req.on('data', chunk => {
-      console.log(`Data chunk available: ${chunk}`)
-      todos = todos.concat(JSON.parse(chunk))
+      const data = JSON.parse(chunk)
+      data.completed = false
+      const newTodo = new Todo(data)
+      newTodo.save((err, newTodo) => {
+        if (err) {
+          console.error(err)
+        }
+      })
     })
-    res.end(JSON.stringify(todos))
+
+    res.end()
   }
 
   res.on('error', (err) => {
