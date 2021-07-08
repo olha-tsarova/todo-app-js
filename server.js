70a610bee86c73d7bfe95ae5747d75aa912c853b
OlgaTs <alita.jet@gmail.com>
2021-07-08 16:06:24 +0300

    CRUD added

--- server.js
+++ server.js
@@ -3,52 +3,86 @@ const http = require('http')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true})
 
-const db = mongoose.connection;
+const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function() {
   console.log('Connected')
-});
+})
 
 const todoSchema = new mongoose.Schema({
   title: String,
-});
+  completed: {
+    type: Boolean,
+    default: false
+  }
+})
 
 const Todo = mongoose.model('Todo', todoSchema)
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
-  console.log(`method: ${method} url: ${url}`);
+  console.log(`method: ${method} url: ${url}`)
 
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PATCH')
 
   if (method === 'GET' && url === '/todos') {
-    res.write()
-    res.end()
+    await Todo.find({})
+      .then(result => {
+        res.end(JSON.stringify(result))
+      })
   }
 
   if (method === 'POST' && url === '/addtodo') {
     req.on('data', chunk => {
       const data = JSON.parse(chunk)
-      data.completed = false
       const newTodo = new Todo(data)
       newTodo.save((err, newTodo) => {
         if (err) {
           console.error(err)
+          res.end(err)
         }
       })
     })
 
-    res.end()
+    res.end('added to list')
   }
 
-  res.on('error', (err) => {
-    console.error(err);
-  })
+  if (method === 'DELETE') {
+    req.on('data', chunk => {
+      const data = JSON.parse(chunk)
+      data.forEach(element => {
+        Todo.deleteOne({ _id: element._id }, function(err) {
+          if (err) {
+            console.error(err)
+            res.end(err)
+          }
+        })
+      });
+    })
 
-  res.end('')
+    res.end('deleted from list')
+  }
+
+  if (method === 'PATCH') {
+    req.on('data', chunk => {
+      const data = JSON.parse(chunk)
+      data.forEach(element => {
+        Todo.findByIdAndUpdate({ _id: element._id }, { title: element.title, completed: element.completed }, {new: true}, (err, dat) => {
+          if (err) {
+            console.error(err)
+            res.end(err)
+          } else {
+            console.log(dat);
+          }
+        })
+      })
+
+      res.end('updated')
+    })
+  }
 })
 
 server.listen(5050)
